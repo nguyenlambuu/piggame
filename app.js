1956b69479556cf63cf069f0daa91eea830e32d9
buunguyenlam <nguyenlambuu@gmail.com>
2018-08-30 16:43:14 +0700

    Add nextPlayer function

--- app.js
+++ app.js
@@ -29,3 +29,20 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
         nextPlayer();
     }
 });
+
+
+
+function nextPlayer() {
+	// Next player
+	activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+	roundScore = 0;
+
+	document.getElementById("current-0").textContent = "0";
+	document.getElementById("current-1").textContent = "0";
+
+	document.querySelector(".player-0-panel").classList.toggle("active");
+	document.querySelector(".player-1-panel").classList.toggle("active");
+
+	document.querySelector(".dice").style.display = "none";
+}
+
